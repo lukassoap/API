c5f12f350cd4e4f694fb0e4d6c1981ce5fe7e3c4
Lucas Ruiz <yacogamer03@gmail.com>
2026-06-29 17:47:26 -0500

    Fast API - Parámetros de ruta y de consulta

--- main.py
+++ main.py
@@ -8,3 +8,18 @@ def mimensaje():
 @app.get("/") 
 def read_root():
     return {"message": mimensaje()}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+   return {"item_id": item_id}
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}, {"item_name": "Qux"}, {"item_name": "Quux"}, {"item_name": "Corge"}, {"item_name": "Grault"}, {"item_name": "Garply"}, {"item_name": "Waldo"}, {"item_name": "Fred"}, {"item_name": "Plugh"}, {"item_name": "Xyzzy"}, {"item_name": "Thud"}]
+
+
+
+@app.get("/items/")
+def read_item(skip: int = 0, limit: int = 10, q: str | None = None):
+   results = fake_items_db[skip : skip + limit]
+   if q:
+      results.append({"item_name": q})
+   return results
